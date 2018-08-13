4d3f7c87ef4372025ef0b85723664ea099ba2887
Attila Satan <attila.satan@gmail.com>
2018-08-13 04:59:49 +0300

    integration specs

---
+++ api_integration_test.go
@@ -0,0 +1,51 @@
+package nanapp
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+
+	"github.com/attilasatan/nanapp/structure"
+
+	"github.com/kataras/iris/httptest"
+)
+
+func TestAPIIntegration(t *testing.T) {
+	err := Init("data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := httptest.New(t, App)
+
+	e.GET("/").Expect().Status(httptest.StatusOK).
+		ContentType("text/html", "utf-8").Body().Equal("<h1>Hello Nanocorp!</h1>")
+
+	cur, err := Collection.Find(nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var res []*structure.Campaign = make([]*structure.Campaign, 0)
+	for cur.Next(nil) {
+		camp := &structure.Campaign{}
+		err := cur.Decode(camp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res = append(res, camp)
+	}
+
+	campaignJSON, err := json.Marshal(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e.GET("/api/campaign").Expect().Status(httptest.StatusOK).
+		Body().Equal(string(campaignJSON))
+
+	// e.GET("/hello").Expect().Status(httptest.StatusOK).
+	// 	JSON().Object().Value("message").Equal("Hello Iris!")
+
+	// e.GET("/custom_path").Expect().Status(httptest.StatusOK).
+	// 	Body().Equal("hello from the custom handler without following the naming guide")
+}
